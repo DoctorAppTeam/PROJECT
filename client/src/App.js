28e29bbec9e1fde5c3d09f998a581be676fe63a1
anjali22Cp <anjali.20201cse0257@presidencyuniversity.in>
2023-12-24 15:34:23 +0530

    Latest commit with Node.js project files

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,8 @@ import PublicRoute from "./components/PublicRoute";
 import ProtectedRoute from "./components/ProtectedRoute";
 import ApplyDoctor from "./pages/ApplyDoctor";
 import NotificationPage from "./pages/NotificationPage";
+import Users from "./pages/admin/Users";
+import Doctors from "./pages/admin/Doctors";
 
 function App() {
   const { loading } = useSelector((state) => state.alerts);
@@ -35,6 +37,25 @@ function App() {
                 </ProtectedRoute>
               }
             />
+
+            <Route
+              path="/admin/users"
+              element={
+                <ProtectedRoute>
+                  <Users />
+                </ProtectedRoute>
+              }
+            />
+
+            <Route
+              path="/admin/doctors"
+              element={
+                <ProtectedRoute>
+                  <Doctors />
+                </ProtectedRoute>
+              }
+            />
+
             <Route
               path="/notification"
               element={
